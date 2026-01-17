6c6c462adec516b4990c5ba21c52a4e88eb04928
helpsolve <junkmail3@helpsolve.org>
2026-01-17 17:03:08 -0500

    Create trigger-util.js

---
+++ custom/trigger-util.js
@@ -0,0 +1,8 @@
+// custom/trigger-utils.js - Future expansion (e.g., WS listener for director triggers)
+
+// Example: Listen for custom WS messages (hook later if needed)
+// window.addEventListener('message', e => {
+//   if (e.data?.action === 'stinger') playStinger(e.data.index);
+// });
+
+console.log("Trigger utils loaded");
